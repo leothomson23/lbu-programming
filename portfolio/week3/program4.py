fd9fb0d78df5e97f40deaaf58c0c6637c975b212
leothomson23 <146092733+leothomson23@users.noreply.github.com>
2023-10-31 16:31:38 +0000

    Uploading portfolio work from Week 3

---
+++ portfolio/week3/program4.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    played = 609
+    batted = 1014
+    runs = 48426
+    notout = 162
+
+    battingaverage = runs/(batted-notout)
+
+    print('Batting Average:', battingaverage)
---
+++ portfolio/week3/program5.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    g1 = 113
+    g2 = 175
+    g3= 12
+
+    total = g1+g2+g3
+
+    fullgroups = total//24
+    remainder = total%24
+
+    print("There will be ", fullgroups, " full groups.")
+    print("There will be a smaller group of ", remainder, " students.")
