b249df8816e04867b429eebc0f26e23219e1e3cb
leothomson23 <146092733+leothomson23@users.noreply.github.com>
2023-11-03 00:19:37 +0000

    Uploading portfolio work from Week 6

---
+++ portfolio/week6/program6.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    def decrypt(message, interval):
+        print('Your message decrypted would be: ',message[interval - 1::interval])
+    message = input('Enter the message you want to decrypt: ')
+    interval = int(input('Enter the interval: '))
+
+    decrypt(message, interval)
