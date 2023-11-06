f9aff3b11b262e7e210e620e71c251044699b7b1
leothomson23 <146092733+leothomson23@users.noreply.github.com>
2023-11-06 21:07:23 +0000

    Uploading Week 1 practical work.

---
+++ practical/week1/practicals.py
@@ -0,0 +1,35 @@
+if __name__ == "__main__":
+    #Practical 1
+    print("Hello World!")
+    print("Hello, my name is Leo!")
+    print("I support Leeds United")
+
+    #Practical 2
+    help(print)
+
+    #Practical 3
+    print(45+20)
+
+    #Practical 4
+    print(10 + 20 - 15)
+    print(10 * 5)
+    print(100 / 33)
+    print(100 // 33)
+    print(10 ** 2)
+    print(10 % 3)
+
+    #Practical 5
+    print(10 + 5 * 2)
+    print(10 - 5 * 10 + 5)
+    print(5 * 10 ** 2)
+
+    #Practical 6
+    print((10 + 5) * 2)
+    print(10 - 5 * (10 + 5))
+    print((5 * 10) ** 2)
+
+    #Practical 7
+    print(12 + (5 * 2 + 3))
+    print(12 + (5 * (2 + 3)))
+    
+
