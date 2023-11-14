c17274e1ae7bf9a1c9e83e16c0afe95da14c1e00
leothomson23 <146092733+leothomson23@users.noreply.github.com>
2023-11-14 14:16:42 +0000

    Uploading portfolio work from Week 8

---
+++ portfolio/week8/rot13.py
@@ -0,0 +1,14 @@
+def rot13(phrase):
+    key = "abcdefghijklmnopqrstuvwxyz"
+    value = "nopqrstuvwxyzabcdefghijklm"
+    cypher = dict(zip(key, value))
+    output = ''
+
+    for letter in phrase:
+        output += str(cypher[letter])
+
+    print(phrase, 'converts to', output)
+
+if __name__ == "__main__":
+	phrase = input('Enter a phrase to convert: ')
+	rot13(phrase)
